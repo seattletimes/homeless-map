b7f167c8af95abd1283b19472eabfaf5576c11ed
Alec Glassford <glassford@cs.stanford.edu>
2018-07-16 17:04:08 -0700

    Start to add stories (needs redesign)

--- src/js/main.js
+++ src/js/main.js
@@ -15,14 +15,27 @@ var commafy = function commafy(num) {
 //get access to Leaflet and the map
 var elem = document.querySelector("leaflet-map");
 var map = elem.map;
-
 var mainLayer = elem.lookup.mainlayer;
-mainLayer.eachLayer((tractLayer)=> {
+
+mainLayer.eachLayer((tractLayer) => {
   var { pit_2018, pit_2017, pit_percent, total_pop, tract_num } = tractLayer.feature.properties;
+  var storyDetail = "";
+
+
+  if (window.stories[tract_num]) {
+    tractLayer.setStyle({ weight: 2, opacity: 1, color: "red" });
+    var story = window.stories[tract_num];
+    storyDetail = `<div class="story=detail">
+      <h2><a href="${story.url}">${story.headline}</a></h2>
+      <img src="${story.image}" alt="${story.alt}">
+    </div>`;
+  }
+
   tractLayer.bindPopup(`<h1 class="bigheader">Census tract ${tract_num}</h1>
+  ${storyDetail}
   <ul>
-    <li>PIT 2018: ${pit_2018 === 25 ? '25 or less' : commafy(pit_2018)}
-    <li>PIT 2017: ${pit_2017 === 25 ? '25 or less' : commafy(pit_2017)}
+    <li>PIT 2018: ${pit_2018 === 25 ? '25 or fewer' : commafy(pit_2018)}
+    <li>PIT 2017: ${pit_2017 === 25 ? '25 or fewer' : commafy(pit_2017)}
     <li>Percent increase: ${(pit_percent * 100).toFixed(0)}%
     <li>Tract's total population: ${commafy(total_pop)}
   `);
