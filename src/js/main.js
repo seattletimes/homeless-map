14f32c1ca161eb0c6b2013803e7729061a6a5923
Alec Glassford <glassford@cs.stanford.edu>
2018-07-17 11:29:43 -0700

    Changed all the UX things?

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,10 @@
+var $ = require("./lib/qsa");
+var debounce = require("./lib/debounce");
+
 // Expand topojson into geojson and put into geodata
 var topojson = require("topojson-client");
 var expandedData = topojson.feature(topoData, topoData.objects.pit_final);
-document.querySelector("geo-data").innerHTML = JSON.stringify(expandedData);
+$.one("geo-data").innerHTML = JSON.stringify(expandedData);
 
 //load our custom elements
 require("component-leaflet-map");
@@ -12,8 +15,11 @@ var commafy = function commafy(num) {
   return num.toLocaleString("en-US");
 };
 
+// dumb global
+var focusedFromPopup = false;
+
 //get access to Leaflet and the map
-var elem = document.querySelector("leaflet-map");
+var elem = $.one("leaflet-map");
 var map = elem.map;
 var mainLayer = elem.lookup.mainlayer;
 
@@ -23,38 +29,47 @@ mainLayer.eachLayer((tractLayer) => {
   var storyDetail = "";
 
   if (window.stories[tract_num]) {
-    tractLayer.setStyle({ weight: 2, opacity: 1 });
+    tractLayer.setStyle({ weight: 3, opacity: 1 });
     var story = window.stories[tract_num];
-    storyDetail = `<div class="story=detail">
-      <h2><a href="${story.url}">${story.headline}</a></h2>
-      <img src="${story.image}" alt="${story.alt}">
-    </div>`;
+    storyDetail = `<a href="${story.url}">Related story: ${story.headline}</a>`;
     flyToLookup[tract_num] = tractLayer.getBounds().getCenter();
+    tractLayer.on("click", () => {
+      focusedFromPopup = true;
+      $.one(`[data-tract="${tract_num}"]`).focus();
+    });
   }
 
   tractLayer.bindPopup(`<h1 class="bigheader">Census tract ${tract_num}</h1>
-  ${storyDetail}
   <ul>
-    <li>PIT 2018: ${pit_2018 === 25 ? '25 or fewer' : commafy(pit_2018)}
-    <li>PIT 2017: ${pit_2017 === 25 ? '25 or fewer' : commafy(pit_2017)}
-    <li>Percent increase: ${(pit_percent * 100).toFixed(0)}%
-    <li>Tract's total population: ${commafy(total_pop)}
-  `);
+    <li>Jan. 2018: ${pit_2018 === 25 ? '25 or fewer' : commafy(pit_2018)} people</li>
+    <li>Jan. 2017: ${pit_2017 === 25 ? '25 or fewer' : commafy(pit_2017)} people</li>
+    <li>Percent change: ${pit_percent > 0 ? '+' : ''}${(pit_percent * 100).toFixed(0)}%</li>
+    <li>Tract's total population: ${commafy(total_pop)}</li>
+  </ul>
+  ${storyDetail}
+  `, { maxWidth: 200 });
 });
 
 map.scrollWheelZoom.disable();
 // Add city/neighborhood labels above tracts
 var topLayer = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}.png', {
-  opacity: 0.8,
+  opacity: 0.6,
   pane: "markerPane",
 }).addTo(map);
 
 
-document.querySelectorAll(".story").forEach((storyDiv) => {
-  var tractNum = storyDiv.getAttribute("data-tract");
-  var flyTo = () => {
-    map.flyTo(flyToLookup[tractNum], 12, { duration: 1 });
+var flyTo = debounce((ev) => {
+  if (focusedFromPopup) {
+    focusedFromPopup = false;
+    return;
   }
-  storyDiv.addEventListener("mouseover", flyTo);
+  var tractNum = ev.target.getAttribute("data-tract");
+  if (map.getCenter() === flyToLookup[tractNum]) return;
+  map.closePopup();
+  map.flyTo(flyToLookup[tractNum], 12.5, { duration: 1 });
+});
+
+$(".story").forEach((storyDiv) => {
+  storyDiv.addEventListener("click", flyTo);
   storyDiv.addEventListener("focus", flyTo);
 });
