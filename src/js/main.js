ecbcbe8392bea7fb587f29a2841f398e3d56d682
Alec Glassford <glassford@cs.stanford.edu>
2018-07-17 18:24:51 -0700

    Update with 'no data' tracts, add other stories

--- src/js/main.js
+++ src/js/main.js
@@ -27,11 +27,14 @@ var flyToLookup = {};
 mainLayer.eachLayer((tractLayer) => {
   var { pit_2018, pit_2017, pit_percent, total_pop, tract_num } = tractLayer.feature.properties;
   var storyDetail = "";
+  if (tract_num === '276') console.log(tractLayer.feature.properties);
 
   if (window.stories[tract_num]) {
     tractLayer.setStyle({ weight: 3, opacity: 1 });
     var story = window.stories[tract_num];
-    storyDetail = `<a href="${story.url}">Related story: ${story.headline}</a>`;
+    storyDetail = `<span class="header block">Related story</span>
+      <a href="${story.url}" target="_blank">${story.headline}</a>
+      <img src="${story.image}" alt="${story.alt}">`;
     flyToLookup[tract_num] = tractLayer.getBounds().getCenter();
     tractLayer.on("click", () => {
       focusedFromPopup = true;
@@ -39,20 +42,27 @@ mainLayer.eachLayer((tractLayer) => {
     });
   }
 
+  var formatCount = function formatCount(num) {
+    return (num === null) ? ("no data") :
+    (`${num === 25 ? '25 or fewer' : commafy(num)} people`);
+  };
+
+  var changeBlock = `<li>
+    <span class="left">Change, 2017-2018: </span>
+    <span class="right">${pit_percent > 0 ? '+' : ''}${commafy(pit_percent * 100)}%${pit_2018 === 25 || pit_2017 === 25 ? '*' : ''}</span>
+  </li>`;
+
   tractLayer.bindPopup(`<h1 class="bigheader">Census tract ${tract_num}</h1>
   <ul>
     <li class="header block">All Home's count of homeless people</li>
     <li>
       <span class="left">Jan. 2018: </span>
-      <span class="right">${pit_2018 === 25 ? '25 or fewer' : commafy(pit_2018)} people</span></li>
+      <span class="right">${formatCount(pit_2018)}</span></li>
     <li>
       <span class="left">Jan. 2017: </span>
-      <span class="right">${pit_2017 === 25 ? '25 or fewer' : commafy(pit_2017)} people</span>
+      <span class="right">${formatCount(pit_2017)}</span>
     </li>
-    <li>
-      <span class="left">Change, 2017-2018: </span>
-      <span class="right">${pit_percent > 0 ? '+' : ''}${commafy(pit_percent * 100)}%</span>
-    </li>
+    ${ pit_2018 === null ? '' : changeBlock}
     <li>&nbsp;</li>
     <li class="block">
       Tract's total population (2017 estimate):<br>
